709ec843344aa56321eced9c693866100847e567
radepal <radepal@gmail.com>
2011-11-27 19:01:59 +0100

    init experiment

---
+++ src/expermient/experiment.java
@@ -0,0 +1,13 @@
+package expermient;
+
+public class experiment {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/expermient/experimentTest.java
@@ -0,0 +1,14 @@
+package expermient;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class experimentTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+}
